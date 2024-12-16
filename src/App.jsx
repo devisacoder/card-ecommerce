406899e3501dd33f2e0aad864ded58fea94489b2
Isabel Caballero <isabelcaballeromotta@hotmail.com>
2024-12-16 16:04:21 -0500

    maquetacion y estilos del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Home } from "./pages/Home/Home"
+
 function App() {
 
   return (
     <>
-      <h1>Card ecommerce</h1>
+      <Home/>
     </>
   )
 }
 
 export default App
+
+// https://www.svgrepo.com/collection/ecommerce-thin-outline-vectors/
